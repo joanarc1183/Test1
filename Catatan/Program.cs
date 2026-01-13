c0d6a088de5c4d9043dcfef4b4764df1c4171083
joanedna1183 <joanedna11@gmail.com>
2026-01-13 15:00:19 +0700

    catatan

--- Catatan/Program.cs
+++ Catatan/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+interface IWorkable
+{
+    void Work();
+}
+
+interface IFeedable
+{
+    void Eat();
+}
+
+class Human : IWorkable, IFeedable
+{
+    public void Work() { }
+    public void Eat() { }
+}
+
+class Robot : IWorkable
+{
+    public void Work() { }
+}
